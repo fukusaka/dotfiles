28bb5e2449e3a2877c124b0e7e1b4a85a3768e44
Shoichi Fukusaka <fukusaka@xa2.so-net.ne.jp>
2001-10-04 15:10:47 +0000

    シェイプアップ

--- emacs/customize.d/50w3.el
+++ emacs/customize.d/50w3.el
@@ -2,15 +2,10 @@
 ;;
 ;; Emacs/W3 Configuration
 ;;
-(if (featurep 'w3-auto)
-    (cond
-     ((string-match "^20" emacs-version)
-      (require 'w3-auto "w3-auto")
-      (setq w3-default-homepage "http://sheep-poe/~fukusaka")
-      ;;(setq url-proxy-services '(("http"     . "localhost:3128")
-      ;;			     ))
-      ;;(setq url-be-asynchronous t)
-      ;;(setq w3-delay-image-loads t)
-      ;;(setq url-xterm-command "kterm -title %s -ut -e %s %s %s")
-      )))
+(setq w3-default-homepage "http://sheep-poe/~fukusaka")
+;;(setq url-proxy-services '(("http"     . "localhost:3128")
+;;			     ))
+;;(setq url-be-asynchronous t)
+;;(setq w3-delay-image-loads t)
+;;(setq url-xterm-command "kterm -title %s -ut -e %s %s %s")
 
