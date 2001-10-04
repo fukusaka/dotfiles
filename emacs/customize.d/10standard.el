14362a9a5540fc9974727874daacf7f75cd950a1
Shoichi Fukusaka <fukusaka@xa2.so-net.ne.jp>
2001-10-04 15:12:35 +0000

    .CPP,.gen_h を自動識別モードの追加。

--- emacs/customize.d/10standard.el
+++ emacs/customize.d/10standard.el
@@ -65,6 +65,8 @@
 	 ;;("\\.diff" . moi-patch-view-mode)
 	 ("\\.pgc$" . c-mode)
 	 ("\\.pgcc$" . c++-mode)
+	 ("\\.CPP$" . c++-mode)
+	 ("\\.gen_h$" . c++-mode)
 	 )
        auto-mode-alist))
 
@@ -129,3 +131,20 @@
 
 ;;
 (autoload 'moi::sample-ascii "moi-sample-ascii" "" t)
+
+;; Browser
+(setq browse-url-xterm-program "kterm")
+
+(setq browse-url-xterm-args
+      '("-fl" "-alias-fixed-medium-r-normal--12-*"
+	"-geometry" "119x47+0+0"))
+
+(defun browse-url-w3m-xterm (url &optional new-window)
+  (interactive (browse-url-interactive-arg "w3m URL: "))
+  (if (= 0 (string-match "file:" url))
+      (setq url (substring url 5)))
+  (message url)
+  (apply #'start-process `(,(concat "w3m" url) nil ,browse-url-xterm-program
+             ,@browse-url-xterm-args "-e" "w3m" "-F" ,url)))
+
+(setq browse-url-browser-function 'browse-url-w3m-xterm)
