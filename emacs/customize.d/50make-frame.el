3c4579b5a29f6a076bc3906345469217595f6143
Shoichi Fukusaka <fukusaka@xa2.so-net.ne.jp>
2007-05-27 06:35:00 +0000

    フレームを作成したとき高さと幅を同じに変更する

--- emacs/customize.d/50make-frame.el
+++ emacs/customize.d/50make-frame.el
@@ -32,23 +32,29 @@
     (progn
       (define-key global-map "\C-z51" 'moi::make-frame-3)
       (define-key global-map "\C-z52" 'moi::make-frame-6)
-      (define-key global-map "\C-z50" 'moi::delete-frame-6)
+      (define-key global-map "\C-z50" 'moi::delete-frames)
 
       (setq moi::desktop-max-x 4)
+      (setq moi::desktop-center-x 1)
+      (setq moi::desktop-center-y 2)
 
-      (defun moi::make-frame (x y)
+      (defun moi::make-frame (&optional x y)
 	(let* ((fpar (frame-parameters))
-	       (bw   
-		(if (string-match "^20" emacs-version)
-		    (cdr (assoc 'border-width fpar))
-		  0))
-	       (left (+ x bw (eval (cdr (assoc 'left fpar)))))
-	       (top  (+ y bw (eval (cdr (assoc 'top fpar)))))
-	       (frame (make-frame)))
-	  (sleep-for 0.09)
-	  (modify-frame-parameters frame `((top + ,top) (left + ,left) ,(cons 'font "fontset-standard")))
-	  (sleep-for 0.09)
-	  frame))
+	       (left (cdr (assoc 'left fpar)))
+	       (top  (cdr (assoc 'top fpar)))
+	       (height (cdr (assoc 'height fpar)))
+	       (width  (cdr (assoc 'width fpar))))
+	  (if (string-match "^20" emacs-version)
+	      (let ((bw (cdr (assoc 'border-width fpar))))
+		(setq left (+ left bw))
+		(setq top (+ top bw))))
+	  (if x (setq left (+ left x)))
+	  (if y (setq top (+ top y)))
+	  (let ((default-frame-alist
+		  (append `((height . ,height) (width . ,width)
+			    (top . ,top) (left . ,left))
+			  default-frame-alist)))
+	    (make-frame))))
 
       (defun moi::move-frame (frame x y)
 	(let ((wid (frame-parameter (or frame (selected-frame)) 'outer-window-id))
@@ -66,17 +72,18 @@
       ;;   (moi::make-frame left top)))
 
       (defun moi::make-frame2 (x y)
-	(let ((frame (moi::make-frame 0 0)))
-	  (moi::move-frame frame (+ x 1) (+ y 2))
+	(let ((frame (moi::make-frame)))
+	  (moi::move-frame frame
+			   (+ x moi::desktop-center-x)
+			   (+ y moi::desktop-center-y))
 	  frame))
-	
 
-      (defvar moi::make-frame-6-alist nil)
+      (defvar moi::make-frame-list nil)
 
       (defun moi::make-frame-3 ()
 	(interactive)
-	(if (not moi::make-frame-6-alist)
-	    (setq moi::make-frame-6-alist
+	(if (not moi::make-frame-list)
+	    (setq moi::make-frame-list
 		  (list
 		   (moi::make-frame2 1 0)
 		   ;;(moi::make-frame2 0 1)
@@ -85,12 +92,12 @@
 
       (defun moi::make-frame-6 ()
 	(interactive)
-	(if (not moi::make-frame-6-alist)
-	    (setq moi::make-frame-6-alist
+	(if (not moi::make-frame-list)
+	    (setq moi::make-frame-list
 		  (list
-		   (moi::make-frame2 1 0)
+		   ;;(moi::make-frame2 1 0)
 		   (moi::make-frame2 -1 0)
-		   ;;(moi::make-frame2 0 1)
+		   (moi::make-frame2 0 1)
 		   (moi::make-frame2 0 -1)
 		   (moi::make-frame2 1 1)
 		   (moi::make-frame2 -1 1)
@@ -98,11 +105,11 @@
 		   (moi::make-frame2 -1 -1)
 		   ))))
 
-      (defun moi::delete-frame-6 ()
+      (defun moi::delete-frames ()
 	(interactive)
-	(while moi::make-frame-6-alist
-	  (delete-frame (car moi::make-frame-6-alist))
-	  (setq moi::make-frame-6-alist (cdr moi::make-frame-6-alist))))
+	(while moi::make-frame-list
+	  (delete-frame (car moi::make-frame-list))
+	  (setq moi::make-frame-list (cdr moi::make-frame-list))))
       ))
 
 ;;; 50make-frame.el ends here
