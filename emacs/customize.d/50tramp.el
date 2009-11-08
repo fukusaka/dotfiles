f56f81881c1729fc04fad7b5db3fa7ea5ee52403
Shoichi Fukusaka <fukusaka@xa2.so-net.ne.jp>
2009-11-08 02:55:55 +0000

    Emacs23/W32

--- emacs/customize.d/50tramp.el
+++ emacs/customize.d/50tramp.el
@@ -5,12 +5,33 @@
 ;; Author: Shoichi Fukusaka <fukusaka@xa2.so-net.ne.jp>
 ;; Keywords: 
 
-(autoload 'tramp-compile "tramp-util")
 
-(define-key global-map "\M-c" 'tramp-compile)
-(define-key global-map "\C-zc" 'tramp-compile)
+(when (<= emacs-major-version 22)
+  (autoload 'tramp-compile "tramp-util")
+  (define-key global-map "\M-c" 'tramp-compile)
+  (define-key global-map "\C-zc" 'tramp-compile))
 
-(setq tramp-default-method "sshx")
+(cond
+
+ ((eq system-type 'windows-nt)
+
+  (setq tramp-default-user "shoichi")
+  ;; minibufferで入力するときユーザ名の最後の１文字が落る
+  ;; login 処理に不具合い？かなぁ。
+  (setq tramp-debug-buffer t)
+  (setq tramp-verbose 10)
+
+  (cond
+   ((executable-find "plink")
+    (setq tramp-default-method "plink")
+    (setq tramp-completion-without-shell-p t)
+    (modify-coding-system-alist 'process "plink" 'utf-8-unix)
+    ))
+  )
+
+ ;; その他
+ (t
+  (setq tramp-default-method "ssh")))
 
 
 
