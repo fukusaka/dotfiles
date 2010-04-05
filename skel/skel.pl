7ff67b5c44fe73a147a416c6a1e465c8884aea93
Shoichi Fukusaka <fukusaka@xa2.so-net.ne.jp>
2010-04-05 09:57:28 +0000

    Perl の定型コードの変更

--- skel/skel.pl
+++ skel/skel.pl
@@ -1,22 +1,9 @@
 #!/usr/bin/perl -T
-# $Id$
-
-# @@FNAME@@ --
-#
-# Copyright (C) @@YEAR@@ @@NAME@@
-#
-# Author: @@NAME@@ <@@ADDRESS@@>
-# Maintainer: @@NAME@@ <@@ADDRESS@@>
-# Created: @@DATE@@
-# Version: 1.0
-# Keywords: 
-
-@@LICENSE@@
-
 use strict;
 use warnings;
 use utf8;                     # ソースコードのencoding指定
 use open ':locale';           # 暗黙に:stdも呼ばれる
+#use Data::Dumper;
 
 #use open ':encoding(utf8)';  # I/OのencodingをUTF8固定にする
 #use open ':std';             # STD三兄弟のencoding指定
@@ -25,11 +12,6 @@ use open ':locale';           # 暗黙に:stdも呼ばれる
 
 @@@@
 FNAME: (file-name-nondirectory buffer-file-name)
-NAME: programmer-name
-ADDRESS: programmer-mail-address
-YEAR: (format-time-string "%Y")
-DATE: (format-time-string "%d %b %Y")
-LICENSE: (moi::ask-license-string "# ")
 @@@@
 
 # @@FNAME@@ ends here
