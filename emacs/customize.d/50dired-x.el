72d2d9b6c1a4a5eff7a667863bb863a973743528
Shoichi Fukusaka <fukusaka@xa2.so-net.ne.jp>
2010-04-07 17:39:27 +0000

    diredの改善

--- emacs/customize.d/50dired-x.el
+++ emacs/customize.d/50dired-x.el
@@ -3,16 +3,16 @@
 ;;
 
 ;; 追加の省略する拡張子
-(setq moi::dired-omit-extensions
+(setq my-dired-omit-extensions
       '(".o" ".elc" "~" ".bin" ".lbin" ".fasl"
 	".a" ".ln" ".fmt" ".lo" ".flc" ".flh" ))
 
 ;; 表示を省略するファイルと拡張子の設定
 (cond
  ((eq system-type 'darwin)
-  (setq moi::dired-omit-files "^#\\|^\\.\\|^Desktop D[BF]$\\|Icon\015"))
+  (setq my-dired-omit-files "^#\\|^\\.\\|^Desktop D[BF]$\\|Icon\015"))
  (t
-  (setq moi::dired-omit-files "^#\\|^\\.")))
+  (setq my-dired-omit-files "^#\\|^\\.")))
 
 ;; GNU Emacs に追加バインド
 (autoload 'dired-jump "dired-x" nil t nil)
@@ -20,22 +20,24 @@
 (define-key global-map "\C-x\C-j" 'dired-jump)
 (define-key global-map "\C-x4\C-j" 'dired-jump-other-window)
 
-;; for emacs 22
-(if (>= emacs-major-version 22)
-    (defalias 'dired-omit-toggle 'dired-omit-mode))
-
-;;
+;; ロード時に設定を行う
 (add-hook 'dired-load-hook
 	  '(lambda ()
-	     (require 'dired-x)
+             (load "dired-x")
+
+             ;; emacs 21以前への対応
+             ;; (dired-omit-toggle => dired-omit-mode)
+             (if (and (not (fboundp 'dired-omit-mode))
+                      (fboundp 'dired-omit-toggle))
+                 (defalias 'dired-omit-mode 'dired-omit-toggle))
 
 	     ;; モードキーの設定
-	     (define-key dired-mode-map "\M-o" 'dired-omit-toggle)
+	     (define-key dired-mode-map "\M-o" 'dired-omit-mode)
 	     (define-key dired-mode-map "f" 'dired-do-shell-command)
 	     (define-key dired-mode-map "U" 'dired-unmark-all-files-no-query)
 
-             ;; emacs 22 以降
-             (when (>= emacs-major-version 22)
+             ;; wdired (writable dired) があれば設定
+             (when (locate-library "wdired")
                (require 'wdired)
                (define-key dired-mode-map "r" 'wdired-change-to-wdired-mode))
 
@@ -44,11 +46,12 @@
 	     ;;(setq dired-guess-shell-znew-switches t)
 	     ;;(setq dired-x-hands-off-my-keys nil)
 
-	     (setq dired-omit-files moi::dired-omit-files)
-	     (setq dired-omit-extensions
-		   (append moi::dired-omit-extensions
-			   dired-omit-extensions))
-	     ))
+	     (setq dired-omit-files my-dired-omit-files)
+             (dolist (ext dired-omit-extensions)
+               (add-to-list 'dired-omit-extensions ext t))
 
-;; 初期で omit 状態
-(add-hook 'dired-mode-hook '(lambda () (dired-omit-toggle)))
+             ;; 初期で omit 状態
+             (add-hook 'dired-mode-hook '(lambda () (dired-omit-mode)))
+
+             ))
+
