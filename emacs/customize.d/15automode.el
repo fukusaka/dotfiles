ac8598ac42e3f681642c24cd210f4da4750c11ce
Shoichi Fukusaka <fukusaka@xa2.so-net.ne.jp>
2010-04-07 17:39:37 +0000

    automodeの改善

--- emacs/customize.d/15automode.el
+++ emacs/customize.d/15automode.el
@@ -2,30 +2,32 @@
 ;; 自動識別するモードの設定
 ;;
 
-(setq auto-mode-alist
-      (append
-       '(
-	 ("\\.h\\'" . c++-mode)
-	 ("\\.pl\\'" . perl-mode)
-	 ("\\.y\\'" . bison-mode)
-	 ("\\.l\\'" . flex-mode)
-	 ("\\.applescript$" . applescript-mode)
+(dolist (e (nreverse
+            '(("\\.h\\'" . c++-mode)
+              ("\\.pl\\'" . perl-mode)
+              ("\\.y\\'" . bison-mode)
+              ("\\.l\\'" . flex-mode)
+              ("\\.applescript$" . applescript-mode)
 
-	 ;; DOS/Windows 系だと大文字になってる場合がある
-	 ("\\.CPP\\'" . c++-mode)
+              ;; DOS/Windows 系だと大文字になってる場合がある
+              ("\\.CPP\\'" . c++-mode)
 
-	 ;; 差分に色づけする
-	 ("patch" . my-patch-view-mode)
-	 ("\\.diff" . my-patch-view-mode)
+              ;; 差分に色づけする
+              ("patch" . my-patch-view-mode)
+              ("\\.diff" . my-patch-view-mode)
 
-	 ;; epcg ? 用
-	 ("\\.pgc\\'" . c-mode)
-	 ("\\.pgcc\\'" . c++-mode)
-	 )
-       auto-mode-alist))
+              ;; epcg ? 用
+              ("\\.pgc\\'" . c-mode)
+              ("\\.pgcc\\'" . c++-mode))))
+  (add-to-list 'auto-mode-alist e))
 
-(autoload 'bison-mode "bison-mode" "bison" t)
-(autoload 'flex-mode "flex-mode" "flex" t)
-(autoload 'applescript-mode "applescript-mode" "major mode for editing AppleScript source." t)
+(autoload 'bison-mode "bison-mode"
+  "bison" t)
+
+(autoload 'flex-mode "flex-mode"
+  "flex" t)
+
+(autoload 'applescript-mode "applescript-mode"
+  "major mode for editing AppleScript source." t)
 
 (autoload 'my-patch-view-mode "my-patch-view")
