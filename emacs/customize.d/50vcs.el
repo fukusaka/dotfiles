21350f4b93579ae053f63f4af3dc9e6e67ca0762
Shoichi Fukusaka <fukusaka@xa2.so-net.ne.jp>
2010-04-08 18:33:16 +0000

    Windowsのみに変更

--- emacs/customize.d/50vcs.el
+++ emacs/customize.d/50vcs.el
@@ -8,10 +8,13 @@
 ;; for Subversion
 (when (locate-library "psvn")
   (autoload 'svn-status "psvn" nil t)
-  ;; サーバ側のコメントはUTF-8で書く運用
-  (setq svn-status-svn-file-coding-system 'utf-8)
-  (setq svn-status-svn-process-coding-system 'utf-8)
-  (modify-coding-system-alist 'process "svn" 'utf-8) ;; svn-diff 文字化け対応
+
+  (when (eq system-type 'windows-nt)
+    ;; サーバ側のコメントはUTF-8で書く運用
+    (setq svn-status-svn-file-coding-system 'utf-8)
+    (setq svn-status-svn-process-coding-system 'utf-8)
+    (modify-coding-system-alist 'process "svn" 'utf-8) ;; svn-diff 文字化け対応
+    )
   )
 
 ;; for Git / git-coreのcontribにある
