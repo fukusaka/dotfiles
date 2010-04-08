177177f51bc73a4c7df43343137341a5ce9d3c6b
Shoichi Fukusaka <fukusaka@xa2.so-net.ne.jp>
2010-04-08 10:13:12 +0000

    NTEmacs で fakecygpty 経由で ssh を使うように変更

--- emacs/customize.d/50tramp.el
+++ emacs/customize.d/50tramp.el
@@ -7,24 +7,13 @@
   (define-key global-map "\M-c" 'tramp-compile)
   (define-key global-map "\C-zc" 'tramp-compile))
 
-(cond
+(setq tramp-default-method "sshx")
 
- ((eq system-type 'windows-nt)
-
-  (setq tramp-default-user "shoichi")
-  ;; minibufferで入力するときユーザ名の最後の１文字が落る
-  ;; login 処理に不具合い？かなぁ。
-  (setq tramp-debug-buffer t)
-  (setq tramp-verbose 10)
-
-  (cond
-   ((executable-find "plink")
-    (setq tramp-default-method "plink")
-    (setq tramp-completion-without-shell-p t)
-    (modify-coding-system-alist 'process "plink" 'utf-8-unix)
-    ))
-  )
-
- ;; その他
- (t
-  (setq tramp-default-method "ssh")))
+;; NTEmacsでCygwinのsshを使う。
+;; 必ず、fakecygpty を使うべし
+(when (and (eq system-type 'windows-nt)
+           (executable-find "f_ssh"))
+  (require 'tramp)
+  (dolist (methods tramp-methods)
+    (if (string-equal (cadr (assq 'tramp-login-program methods)) "ssh")
+        (setcdr (assq 'tramp-login-program methods) '("f_ssh")))))
