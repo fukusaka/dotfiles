bc87b86e904ef2843c199b1bd9f48d9571de12c3
Shoichi Fukusaka <fukusaka@xa2.so-net.ne.jp>
2010-04-08 10:23:59 +0000

    修正

--- emacs/customize.d/50tramp.el
+++ emacs/customize.d/50tramp.el
@@ -7,13 +7,12 @@
   (define-key global-map "\M-c" 'tramp-compile)
   (define-key global-map "\C-zc" 'tramp-compile))
 
-(setq tramp-default-method "sshx")
-
 ;; NTEmacsでCygwinのsshを使う。
 ;; 必ず、fakecygpty を使うべし
 (when (and (eq system-type 'windows-nt)
            (executable-find "f_ssh"))
   (require 'tramp)
+  (setq tramp-default-method "sshx")
   (dolist (methods tramp-methods)
     (if (string-equal (cadr (assq 'tramp-login-program methods)) "ssh")
         (setcdr (assq 'tramp-login-program methods) '("f_ssh")))))
