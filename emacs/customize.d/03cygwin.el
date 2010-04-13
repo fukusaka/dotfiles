364443826d4c8e70106bbd06c26ee9c810468d3b
Shoichi Fukusaka <fukusaka@xa2.so-net.ne.jp>
2010-04-13 18:13:05 +0000

    symbolic link の追加/Cygwin修正

--- emacs/customize.d/03cygwin.el
+++ emacs/customize.d/03cygwin.el
@@ -2,16 +2,27 @@
 ;; Windows系でCygwin対応コード
 ;;
 
-(when (or (eq system-type 'windows-nt)
-          (eq system-type 'cygwin))
+(when (memq system-type '(cygwin windows-nt))
+
+  ;; cygwin のパスを通す
+  (defvar cygwin-top-directory "C:/cygwin/")
+
+  (add-to-list
+   'exec-path
+   (expand-file-name (concat cygwin-top-directory "bin")) t)
+
+  ;; Cygwin の Info 追加
+  (add-to-list
+   'Info-default-directory-list
+   (expand-file-name (concat cygwin-top-directory "usr/share/info")) t)
 
   ;; cygwin のバージョン取得
   (let* ((text (shell-command-to-string "cygcheck.exe -cd cygwin"))
          (version
           (if (and text
-                   (string-match ".*cygwin +\\\([0-9.]+\\\).*" text))
+                   (string-match ".*cygwin +\\([0-9.]+\\).*" text))
               (match-string 1 text)))
-         (vers (if version (split-string version "\\\.")))
+         (vers (if version (split-string version "\\.")))
          (major (if (listp vers) (nth-value 1 vers)))
          (minor (if (listp vers) (nth-value 2 vers))))
 
@@ -21,7 +32,7 @@
 
   (defun setenv-cygwin (value)
     (let* ((func '(lambda (val)
-                    (string-match "\\\(no\\\)?\\\([^:]*\\\)\\\(:.*\\\)?" val)
+                    (string-match "\\(no\\)?\\([^:]*\\)\\(:.*\\)?" val)
                     (match-string 2 val)))
            (env (getenv "CYGWIN"))
            (envs (if env (split-string env " ")))
@@ -67,4 +78,36 @@
     (if locale-coding-system
         (setq default-file-name-coding-system locale-coding-system))
     )
+
+  ;; cygwin-mount.el
+  (require 'cygwin-mount)
+  (cygwin-mount-activate)
+
+;;; Follow Cygwin symlinks.
+;;; Handles old-style (text file) symlinks and new-style (.lnk file) symlinks.
+;;; (Non-Cygwin-symlink .lnk files, such as desktop shortcuts, are still loaded as such.)
+  (defun follow-cygwin-symlink ()
+    "Follow Cygwin symlinks.
+Handles old-style (text file) and new-style (.lnk file) symlinks.
+\(Non-Cygwin-symlink .lnk files, such as desktop shortcuts, are still
+loaded as such.)"
+    (save-excursion
+      (goto-char 0)
+      (cond
+       ((looking-at
+         "L\x000\x000\x000\x001\x014\x002\x000\x000\x000\x000\x000\x0C0\x000\x000\x000\x000\x000\x000\x046\x00C")
+        (re-search-forward
+         "\x000\\([-A-Za-z0-9_\\.\\\\\\$%@(){}~!#^'`][-A-Za-z0-9_\\.\\\\\\$%@(){}~!#^'`]+\\)")
+        (find-alternate-file (match-string 1))) ;; ?
+       ((looking-at "!<symlink>\377\376")
+        (re-search-forward "!<symlink>\\(\377\376.*\\)")
+        (find-alternate-file
+         (decode-coding-string (match-string 1) 'utf-16)))
+       ((looking-at "!<symlink>")
+        (re-search-forward "!<symlink>\\(.*\\)\0")
+        (find-alternate-file (match-string 1)))
+       )))
+
+  (add-hook 'find-file-hooks 'follow-cygwin-symlink)
+
   )
