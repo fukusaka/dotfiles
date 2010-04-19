f66ba275069a03ef7cc530710c4acd8efc399d12
Shoichi Fukusaka <fukusaka@xa2.so-net.ne.jp>
2010-04-19 18:54:46 +0000

    Python のスケルトン追加

---
+++ skel/skel.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# $Id: skel.pl,v 1.3 1999/07/20 07:40:52 fukusaka Exp $
+
+# @@FNAME@@ --
+#
+# Copyright (C) @@YEAR@@ @@NAME@@
+#
+# Author: @@NAME@@ <@@ADDRESS@@>
+# Maintainer: @@NAME@@ <@@ADDRESS@@>
+# Created: @@DATE@@
+# Version: 1.0
+# Keywords: 
+
+# This file is part of 
+
+@@LICENSE@@
+
+@@@@
+FNAME: (file-name-nondirectory buffer-file-name)
+NAME: programmer-name
+ADDRESS: programmer-mail-address
+YEAR: (format-time-string "%Y")
+DATE: (format-time-string "%d %b %Y")
+LICENSE: (moi::license-string 'GPL "# " t)
+@@@@
+
+# @@FNAME@@ ends here
