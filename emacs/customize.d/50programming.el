9cb459701ca13381a5f2aed477bc5572b5958300
Shoichi Fukusaka <fukusaka@xa2.so-net.ne.jp>
2010-04-20 17:20:43 +0000

    gtagsの追加

--- emacs/customize.d/50programming.el
+++ emacs/customize.d/50programming.el
@@ -7,7 +7,7 @@
 ;; 行末スペース削除支援
 (setq-default show-trailing-whitespace t)	;; 行末の不要スペースを強調表示
 ;;(add-hook 'before-save-hook
-;;          'delete-trailing-whitespace)		;; 保存時に無駄なスペースを削除
+;;          'delete-trailing-whitespace)	;; 保存時に無駄なスペースを削除
 
 
 (defun my-not-require-final-newline ()
@@ -20,6 +20,16 @@
 ;;(which-function-mode)
 
 (setq vc-follow-symlinks t)
+
+;; gtags
+(autoload 'gtags-mode "gtags" "" t)
+(setq gtags-mode-hook
+      '(lambda ()
+         (local-set-key "\M-t" 'gtags-find-tag)
+         (local-set-key "\M-r" 'gtags-find-rtag)
+         (local-set-key "\M-s" 'gtags-find-symbol)
+         (local-set-key "\C-t" 'gtags-pop-stack)
+         ))
 
 (add-hook 'c-mode-common-hook
           '(lambda ()
@@ -39,16 +49,23 @@
  'c-mode-common-hook
  '(lambda ()
     (setq c-font-lock-extra-types (append c-font-lock-extra-types glib-types))
-    (setq c++-font-lock-extra-types (append c++-font-lock-extra-types glib-types))))
+    (setq c++-font-lock-extra-types (append c++-font-lock-extra-types glib-types))
+    (gtags-mode 1)
+    ))
 
 ;; Emacs 22 以降の対応
 (when (and (>= emacs-major-version 22)
            window-system)
+
   ;; Yasnippet
-  ;;(require 'yasnippet-bundle)
+  (require 'yasnippet-bundle)
 
   (require 'auto-complete-config)
   (ac-config-default)
+
+  ;;  (setq ac-auto-start 3)
+  (setq ac-auto-start nil)
+  (setq ac-auto-show-menu nil)
 
   ;; Color-moccur
   (require 'color-moccur)
