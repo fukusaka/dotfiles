e5142f801fa5c6c642cfd55f44be060c258f810f
Shoichi Fukusaka <fukusaka@xa2.so-net.ne.jp>
2010-04-22 17:05:39 +0000

    add-to-list はシンボルが束縛されていないと動作しないので、評価の順序を変更

--- emacs/customize.d/50tramp.el
+++ emacs/customize.d/50tramp.el
@@ -10,20 +10,23 @@
 ;; NTEmacsでCygwinのsshを使う。
 ;; 必ず、fakecygpty を使うべし
 (eval-after-load "tramp"
-  '(when (and (eq system-type 'windows-nt)
-              (executable-find "f_ssh"))
-     (require 'tramp)
-     (dolist (methods tramp-methods)
-       (if (string-equal (cadr (assq 'tramp-login-program methods)) "ssh")
-           (setcdr (assq 'tramp-login-program methods) '("f_ssh"))))))
+  '(progn
+     (when (and (eq system-type 'windows-nt)
+		(executable-find "f_ssh"))
+       (require 'tramp)
+       (dolist (methods tramp-methods)
+	 (if (string-equal (cadr (assq 'tramp-login-program methods)) "ssh")
+	     (setcdr (assq 'tramp-login-program methods) '("f_ssh")))))
 
-;; ローカルアクセスには直接接続する
-(add-to-list 'tramp-default-proxies-alist '("localhost" nil nil) t)
-(add-to-list 'tramp-default-proxies-alist '((regexp-quote (system-name)) nil nil) t)
+     ;; ローカルアクセスには直接接続する
+     (add-to-list 'tramp-default-proxies-alist '("localhost" nil nil) t)
+     (add-to-list 'tramp-default-proxies-alist '((regexp-quote (system-name)) nil nil) t)
 
-;; root アクセスは常に sudo を使う
-(add-to-list 'tramp-default-method-alist '(nil "\\`root\\'" "sudo") t)
-(add-to-list 'tramp-default-proxies-alist '(nil "\\`root\\'" "/ssh:%h:") t)
+     ;; root アクセスは常に sudo を使う
+     (add-to-list 'tramp-default-method-alist '(nil "\\`root\\'" "sudo") t)
+     (add-to-list 'tramp-default-proxies-alist '(nil "\\`root\\'" "/ssh:%h:") t)
 
-;; 特定のホスト群には途中を経由する
-;;(add-to-list 'tramp-default-proxies-alist '("\\`far-host\\'" nil "/ssh:fukusaka@proxy-host:") t)
+     ;; 特定のホスト群には途中を経由する
+     ;;(add-to-list 'tramp-default-proxies-alist '("\\`far-host\\'" nil "/ssh:fukusaka@proxy-host:") t)
+     ))
+
