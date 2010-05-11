8a5e5aea148daa4f072ba6e9c6491e5371352185
Shoichi Fukusaka <fukusaka@xa2.so-net.ne.jp>
2010-05-11 20:19:44 +0000

    flymake 設定の途中

--- emacs/customize.d/45flymake.el
+++ emacs/customize.d/45flymake.el
@@ -1,21 +1,23 @@
+;; -*-coding: utf-8 -*-
 (require 'flymake)
 
-;;; エラーをミニバッファに表示
-;;;; http://d.hatena.ne.jp/xcezx/20080314/1205475020
-;;(defun flymake-display-err-minibuf ()
-;;  "Displays the error/warning for the current line in the minibuffer"
-;;  (interactive)
-;;  (let* ((line-no             (flymake-current-line-no))
-;;         (line-err-info-list  (nth 0 (flymake-find-err-info flymake-err-info line-no)))
-;;         (count               (length line-err-info-list)))
-;;    (while (> count 0)
-;;      (when line-err-info-list
-;;        (let* ((file       (flymake-ler-file (nth (1- count) line-err-info-list)))
-;;               (full-file  (flymake-ler-full-file (nth (1- count) line-err-info-list)))
-;;               (text (flymake-ler-text (nth (1- count) line-err-info-list)))
-;;               (line       (flymake-ler-line (nth (1- count) line-err-info-list))))
-;;          (message "[%s] %s" line text)))
-;;      (setq count (1- count)))))
+
+; エラーをミニバッファに表示
+;; http://d.hatena.ne.jp/xcezx/20080314/1205475020
+(defun flymake-display-err-minibuf ()
+  "Displays the error/warning for the current line in the minibuffer"
+  (interactive)
+  (let* ((line-no             (flymake-current-line-no))
+         (line-err-info-list  (nth 0 (flymake-find-err-info flymake-err-info line-no)))
+         (count               (length line-err-info-list)))
+    (while (> count 0)
+      (when line-err-info-list
+        (let* ((file       (flymake-ler-file (nth (1- count) line-err-info-list)))
+               (full-file  (flymake-ler-full-file (nth (1- count) line-err-info-list)))
+               (text (flymake-ler-text (nth (1- count) line-err-info-list)))
+               (line       (flymake-ler-line (nth (1- count) line-err-info-list))))
+          (message "[%s] %s" line text)))
+      (setq count (1- count)))))
 
 (add-hook 'find-file-hook 'flymake-find-file-hook)
 
@@ -30,3 +32,22 @@
 
 ;;シンタックスチェックは次のコマンドが呼ばれる
 ;;make -s -C . CHK_SOURCES=hoge.cpp SYNTAX_CHECK_MODE=1 check-syntax
+
+;; flymake 現在行のエラーをpopup.elのツールチップで表示する
+(defun flymake-display-err-menu-for-current-line ()
+  (interactive)
+  (let* ((line-no             (flymake-current-line-no))
+         (line-err-info-list  (nth 0 (flymake-find-err-info flymake-err-info line-no))))
+    (when line-err-info-list
+      (let* ((count           (length line-err-info-list))
+             (menu-item-text  nil))
+        (while (> count 0)
+          (setq menu-item-text (flymake-ler-text (nth (1- count) line-err-info-list)))
+          (let* ((file       (flymake-ler-file (nth (1- count) line-err-info-list)))
+                 (line       (flymake-ler-line (nth (1- count) line-err-info-list))))
+            (if file
+                (setq menu-item-text (concat menu-item-text " - " file "(" (format "%d" line) ")"))))
+          (setq count (1- count))
+          (if (> count 0) (setq menu-item-text (concat menu-item-text "\n")))
+          )
+        (popup-tip menu-item-text)))))
