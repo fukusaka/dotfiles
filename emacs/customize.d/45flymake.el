d22261097f3d76150a4e84de1ae11c0d341464c3
Shoichi Fukusaka <fukusaka@xa2.so-net.ne.jp>
2010-05-13 20:28:33 +0000

    flymake の設定 popup.el tipを使う設定

--- emacs/customize.d/45flymake.el
+++ emacs/customize.d/45flymake.el
@@ -21,71 +21,57 @@
   (global-set-key "\M-p" 'flymake-goto-prev-error)
   (global-set-key "\M-n" 'flymake-goto-next-error)
 
-  ;;(global-set-key "\M-p" 'flymake-display-err-minibuf)
-  ;;(global-set-key "\M-n" 'flymake-display-err-menu-for-current-line)
+  ;; 警告エラー行の表示
+  (global-set-key "\C-cd" 'flymake-display-err-menu-for-current-line)
 
   (defun my-flymake-popup-menu (menu-data)
     (let* ((menu-title     (nth 0 menu-data))
 	   (menu-items     (nth 1 menu-data))
 	   (menu-commands  (mapcar (lambda (foo)
-				     (cons (nth 0 foo) (nth 1 foo)))
+				     (nth 0 foo))
 				   menu-items)))
-      (popup-menu* menu-items)))
+      (popup-tip (mapconcat 'identity (cons menu-title menu-commands) "\n")))
+    nil)
 
   (defadvice flymake-popup-menu
-    (around my-ad-flymake-popup-menu activate)
+    (around my-ad-flymake-popup-menu)
     (let ((menu-data (ad-get-arg 0)))
       (my-flymake-popup-menu menu-data)))
 
-  (defun my-toggle-flymake-popup-menu-use-popup.el ()
-    (interactive)
-    (ad-activate-on my-ad-flymake-popup-menu))
+  (defadvice flymake-display-err-menu-for-current-line
+    (around my-flymake-display-err-menu-for-current-line activate)
+    (ad-activate 'flymake-popup-menu)
+    ad-do-it
+    (ad-deactivate 'flymake-popup-menu)
+    )
 
-
-
-;;;; flymake 現在行のエラーをpopup.elのツールチップで表示する
-;;(defun flymake-display-err-menu-for-current-line ()
-;;  (interactive)
-;;  (let* ((line-no             (flymake-current-line-no))
-;;         (line-err-info-list  (nth 0 (flymake-find-err-info flymake-err-info line-no))))
-;;    (when line-err-info-list
-;;      (let* ((count           (length line-err-info-list))
-;;             (menu-item-text  nil))
-;;        (while (> count 0)
-;;          (setq menu-item-text (flymake-ler-text (nth (1- count) line-err-info-list)))
-;;          (let* ((file       (flymake-ler-file (nth (1- count) line-err-info-list)))
-;;                 (line       (flymake-ler-line (nth (1- count) line-err-info-list))))
-;;            (if file
-;;                (setq menu-item-text (concat menu-item-text " - " file "(" (format "%d" line) ")"))))
-;;          (setq count (1- count))
-;;          (if (> count 0) (setq menu-item-text (concat menu-item-text "\n")))
-;;          )
-;;        (popup-tip menu-item-text)))))
-;;;(defun flymake-cc-init ()
-;;;  (let* ((temp-file   (flymake-init-create-temp-buffer-copy
-;;;                       'flymake-create-temp-inplace))
-;;;         (local-file  (file-relative-name
-;;;                       temp-file
-;;;                       (file-name-directory buffer-file-name))))
-;;;    (list "g++" (list "-Wall" "-Wextra" "-fsyntax-only" local-file))))
-;;;
-;;;(push '("¥¥.cpp$" flymake-cc-init) flymake-allowed-file-name-masks)
-;;;
-
+  ;; Makefile を使わないC/C++のチェック
+  ;;(defun flymake-cc-init ()
+  ;;  (let* ((temp-file   (flymake-init-create-temp-buffer-copy
+  ;;                       'flymake-create-temp-inplace))
+  ;;         (local-file  (file-relative-name
+  ;;                       temp-file
+  ;;                       (file-name-directory buffer-file-name))))
+  ;;    (list "g++" (list "-Wall" "-Wextra" "-fsyntax-only" local-file))))
+  ;;
+  ;;(push '("¥¥.cpp$" flymake-cc-init) flymake-allowed-file-name-masks)
+  ;;
 
   ;; Invoke ruby with '-c' to get syntax checking
-  (defun flymake-ruby-init ()
-    (let* ((temp-file   (flymake-init-create-temp-buffer-copy
-			 'flymake-create-temp-inplace))
-	   (local-file  (file-relative-name
-			 temp-file
-			 (file-name-directory buffer-file-name))))
-      (list "ruby" (list "-c" local-file))))
+  (when (executable-find "ruby")
+    (defun flymake-ruby-init ()
+      (let* ((temp-file   (flymake-init-create-temp-buffer-copy
+			   'flymake-create-temp-inplace))
+	     (local-file  (file-relative-name
+			   temp-file
+			   (file-name-directory buffer-file-name))))
+	(list "ruby" (list "-c" local-file))))
 
-  (push '(".+\\.rb$" flymake-ruby-init) flymake-allowed-file-name-masks)
-  (push '("Rakefile$" flymake-ruby-init) flymake-allowed-file-name-masks)
+    (push '(".+\\.rb$" flymake-ruby-init) flymake-allowed-file-name-masks)
+    (push '("Rakefile$" flymake-ruby-init) flymake-allowed-file-name-masks)
 
-  (push '("^\\(.*\\):\\([0-9]+\\): \\(.*\\)$" 1 2 nil 3) flymake-err-line-patterns)
+    (push '("^\\(.*\\):\\([0-9]+\\): \\(.*\\)$" 1 2 nil 3) flymake-err-line-patterns)
+    )
 
   ;; bash チェック
   (defvar flymake-shell-of-choice
