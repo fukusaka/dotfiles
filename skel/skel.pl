97390597db569b37fdaa653fb8d274c9c95258cb
Shoichi Fukusaka <fukusaka@xa2.so-net.ne.jp>
2010-05-18 15:32:18 +0000

    perl のスケルトンコードの強化

--- skel/skel.pl
+++ skel/skel.pl
@@ -1,17 +1,35 @@
 #!/usr/bin/perl -T
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) @@YEAR@@ @@NAME@@
+#
+# Author: @@NAME@@ <@@ADDRESS@@>
+# Created: @@DATE@@
+# Version: 1.0
+# Keywords:
+#
+@@LICENSE@@
+
 use strict;
 use warnings;
 use utf8;                     # ソースコードのencoding指定
-use open ':locale';           # 暗黙に:stdも呼ばれる
+use open ':encoding(utf8)';   # I/OのencodingをUTF8固定にする
+use open ':std';              # STD三兄弟のencoding指定
 #use Data::Dumper;
 
-#use open ':encoding(utf8)';  # I/OのencodingをUTF8固定にする
-#use open ':std';             # STD三兄弟のencoding指定
+# 安全じゃないBASH変数の削除
+delete @ENV{qw(IFS CDPATH ENV BASH_ENV)};
 
-#use encoding 'utf8';         # でも代用可能(use utf8以下)
+# PATHの明示指定
+$ENV{PATH} = q(/bin:/usr/bin);
+
+# @@FNAME@@ ends here
 
 @@@@
 FNAME: (file-name-nondirectory buffer-file-name)
+NAME: programmer-name
+ADDRESS: programmer-mail-address
+YEAR: (format-time-string "%Y")
+DATE: (format-time-string "%d %b %Y")
+LICENSE: (my-ask-license-string "# ")
 @@@@
-
-# @@FNAME@@ ends here
