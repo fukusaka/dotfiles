5c0b814dcb7ea5723578fa9430605eea255a2982
Shoichi Fukusaka <fukusaka@xa2.so-net.ne.jp>
2010-05-18 15:32:43 +0000

    スケルトン展開の整理

--- emacs/elisp/my-skel-file.el
+++ emacs/elisp/my-skel-file.el
@@ -122,100 +122,31 @@
       (font-lock-fontify-buffer))
   )
 
-(setq my-license-list
-      '(
-	("GPL"
-"This program is free software; you can redistribute it and/or modify"
-"it under the terms of the GNU General Public License as published by"
-"the Free Software Foundation; either version 2 of the License, or"
-"(at your option) any later version."
-""
-"This program is distributed in the hope that it will be useful,"
-"but WITHOUT ANY WARRANTY; without even the implied warranty of"
-"MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the"
-"GNU General Public License for more details."
-""
-"You should have received a copy of the GNU General Public License"
-"along with this program; if not, write to the Free Software"
-"Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA"
-         )
-	("LGPL-2"
-"This library is free software; you can redistribute it and/or"
-"modify it under the terms of the GNU Library General Public"
-"License as published by the Free Software Foundation; either"
-"version 2 of the License, or (at your option) any later version."
-""
-"This library is distributed in the hope that it will be useful,"
-"but WITHOUT ANY WARRANTY; without even the implied warranty of"
-"MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU"
-"Library General Public License for more details."
-""
-"You should have received a copy of the GNU Library General Public"
-"License along with this library; if not, write to the Free"
-"Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA"
-         )
-	("LGPL-2.1"
-"This library is free software; you can redistribute it and/or"
-"modify it under the terms of the GNU Lesser General Public"
-"License as published by the Free Software Foundation; either"
-"version 2 of the License, or (at your option) any later version."
-""
-"This library is distributed in the hope that it will be useful,"
-"but WITHOUT ANY WARRANTY; without even the implied warranty of"
-"MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU"
-"Lesser General Public License for more details."
-""
-"You should have received a copy of the GNU Lesser General Public"
-"License along with this library; if not, write to the Free Software"
-"Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307  USA"
-	 )
-	("BSD"
-"Redistribution and use in source and binary forms, with or without"
-"modification, are permitted provided that the following conditions"
-"are met:"
-"1. Redistributions of source code must retain the above copyright"
-"   notice, this list of conditions and the following disclaimer."
-"2. Redistributions in binary form must reproduce the above copyright"
-"   notice, this list of conditions and the following disclaimer in the"
-"   documentation and/or other materials provided with the distribution."
-""
-"THIS SOFTWARE IS PROVIDED BY THE REGENTS AND CONTRIBUTORS ``AS IS'' AND"
-"ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE"
-"IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE"
-"ARE DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE"
-"FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL"
-"DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS"
-"OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)"
-"HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT"
-"LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY"
-"OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF"
-"SUCH DAMAGE."
-         )
-	))
+(defvar my-license-dir (concat my-top-conf-dir "license/"))
 
 (defun my-license-string (type head &optional flag)
-  (let ((l-list (assoc (if (symbolp type) (symbol-name type) type)
-		       my-license-list))
-	str)
-    (if l-list
-	(progn
-	  (setq l-list (cdr l-list))
-	  (while l-list
-	    (let* ((a (car l-list))
-		   (b (if (not (and flag (string= "" a)))
-			  (concat head a ))))
-	      (setq str (concat str (if str "\n") b)))
-	    (setq l-list (cdr l-list)))
-	  str))
-    ))
+  (let ((license-file (concat my-license-dir type ".txt")))
+    (when (file-exists-p license-file)
+      (with-temp-buffer
+	(insert-file-contents license-file)
+	(goto-char (point-min))
+	(while (re-search-forward "^" nil t)
+	  (replace-match head nil nil))
+	(buffer-string)))))
 
-(defvar my-ask-license-hist '("GPL" "LGPL-2" "LGPL-2.1" "BSD"))
+
+(defvar my-ask-license-hist '("None"))
 
 (defun my-ask-license-string (head &optional flag)
-  (my-license-string
-   (completing-read "License: " my-license-list nil t "BSD"
-		    'my-ask-license-hist)
-   head flag))
+  (let ((my-license-list (mapcar '(lambda (e) (substring e 0 -4))
+				 (directory-files my-license-dir nil "\\.txt\\'"))))
+    (my-license-string
+     (completing-read
+      "License: "
+      (cons "None" my-license-list)
+      nil t "None"
+      'my-ask-license-hist)
+     head flag)))
 
 (defun my-c-include-once-macro (file)
   (let* ((fname (file-name-nondirectory file)) (len (length fname)) (pos 0) str)
