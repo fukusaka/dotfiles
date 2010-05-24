7ed94ee1027241e4f67ab349640a56edd9f1c12f
Shoichi Fukusaka <fukusaka@xa2.so-net.ne.jp>
2010-05-24 06:52:11 +0000

    eldoc 表示の遅延を設定

--- emacs/customize.d/45eldoc.el
+++ emacs/customize.d/45eldoc.el
@@ -2,6 +2,7 @@
 ;; Eldoc を有効化
 (require 'eldoc)
 (require 'eldoc-extension)
+(setq eldoc-idle-delay 2)
 (setq eldoc-echo-area-use-multiline-p t)
 (add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
 (add-hook 'lisp-interaction-mode-hook 'turn-on-eldoc-mode)
