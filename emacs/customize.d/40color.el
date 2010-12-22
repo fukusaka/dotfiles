ca6fad0c953548e4ca3d67144530cd0e3997deee
Shoichi Fukusaka <fukusaka@xa2.so-net.ne.jp>
2010-12-22 16:06:29 +0900

    文字列の色付けの設定整理

--- emacs/customize.d/40color.el
+++ emacs/customize.d/40color.el
@@ -6,13 +6,13 @@
 ;; jit-lock-mode を使う
 (setq font-lock-support-mode 'jit-lock-mode)
 
-(defface moi-string-face
-  '((((class color) (background light)) (:foreground "Brown"))
+;; クォート文字列の色付け
+
+(custom-set-faces
+ '(font-lock-string-face
+   ((((class color) (background light)) (:foreground "Brown"))
     (((class color) (background dark)) (:foreground "Salmon"))
-    (t (:italic t)))
-  nil
-  )
-(setq font-lock-string-face 'moi-string-face)
+    (t (:italic t)))))
 
 ;; 差分表示(diff-mode)のとき色付け
 
