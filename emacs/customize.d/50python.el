e912e3f401e386747321e9a6f2645e81ed56abf2
Shoichi Fukusaka <fukusaka@xa2.so-net.ne.jp>
2011-06-05 03:08:47 +0900

    lambda モードの設定

--- emacs/customize.d/50python.el
+++ emacs/customize.d/50python.el
@@ -1,6 +1,10 @@
 ;;
 (defun my-python-mode-init ()
-
+  ;;(require 'pycomplete)         ;; pymacs を使わないのでまだ保留
+  ;;(require 'lambda-mode)        ;; λはなんか見づらい、、、
+  ;;(lambda-mode)
   )
 
 (add-hook 'python-mode-hook 'my-python-mode-init)
+
+(setq lambda-symbol (string (make-char 'greek-iso8859-7 107)))
