587a8fffcee2a1fca754e8f9bc5046f4789d25a2
Shoichi Fukusaka <fukusaka@xa2.so-net.ne.jp>
2012-01-10 21:53:58 +0900

    apache conf の自動モードの調節

--- emacs/customize.d/15automode.el
+++ emacs/customize.d/15automode.el
@@ -18,7 +18,7 @@
               ("\\.tpl\\'" . smarty-mode)
 
               ("\\.htaccess\\'"   . apache-mode)
-              ("httpd\\.conf\\'"  . apache-mode)
+              ("httpd\\(-.*\\)?\\.conf"  . apache-mode)
               ("srm\\.conf\\'"    . apache-mode)
               ("access\\.conf\\'" . apache-mode)
               ("sites-\\(available\\|enabled\\)/" . apache-mode)
