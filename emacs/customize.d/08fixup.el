912b75d62cc87e14e477402294ed2bfca74342ed
Shoichi Fukusaka <fukusaka@xa2.so-net.ne.jp>
2012-02-13 01:23:01 +0900

    nxhtml を 23.4で使う場合、不要な警告を抑制
    * See http://stackoverflow.com/questions/5468952/how-do-i-hide-emacs-obsolete-variable-warnings

--- emacs/customize.d/08fixup.el
+++ emacs/customize.d/08fixup.el
@@ -69,3 +69,14 @@
    '("Sample Popup"
      ("OK" . t))
    t))
+
+;; Mumamo is making emacs 23.3 freak out:
+(when (and (equal emacs-major-version 23)
+           (>= emacs-minor-version 3))
+  (eval-after-load "bytecomp"
+    '(add-to-list 'byte-compile-not-obsolete-vars
+                  'font-lock-beginning-of-syntax-function))
+  ;; tramp-compat.el clobbers this variable!
+  (eval-after-load "tramp-compat"
+    '(add-to-list 'byte-compile-not-obsolete-vars
+                  'font-lock-beginning-of-syntax-function)))
