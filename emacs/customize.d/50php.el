fd5de172b1fe99c44c30484ced9404638b9aa10a
Shoichi Fukusaka <fukusaka@xa2.so-net.ne.jp>
2012-12-23 23:09:44 +0900

    PHPの配列のインデントを修正

--- emacs/customize.d/50php.el
+++ emacs/customize.d/50php.el
@@ -6,6 +6,20 @@
   (setq-default tab-width 4)
   (setq-default indent-tabs-mode t)
 
+  ;; for Indentation of arrays
+  ;; http://emacswiki.org/emacs/PhpMode
+  (c-set-offset 'arglist-close 0)
+  (defun ywb-php-lineup-arglist-intro (langelem)
+    (save-excursion
+      (goto-char (cdr langelem))
+      (vector (+ (current-column) c-basic-offset))))
+  (defun ywb-php-lineup-arglist-close (langelem)
+    (save-excursion
+      (goto-char (cdr langelem))
+      (vector (current-column))))
+  (c-set-offset 'arglist-intro 'ywb-php-lineup-arglist-intro)
+  (c-set-offset 'arglist-close 'ywb-php-lineup-arglist-close)
+
   )
 
 (add-hook 'php-mode-hook 'my-php-mode-init)
