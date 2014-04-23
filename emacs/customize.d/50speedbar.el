4802d86faa234aceea1dda56559a5faf1327f3f7
Shoichi Fukusaka <fukusaka@xa2.so-net.ne.jp>
2014-04-23 23:25:53 +0900

    speedbar は結局使わないので削除

--- emacs/customize.d/50speedbar.el
+++
@@ -1,8 +0,0 @@
-;; sr-speedbar
-(require 'sr-speedbar)
-
-(setq sr-speedbar-right-side nil)
-(setq sr-speedbar-width-x 45)
-
-(my-prefix-set-key "\C-s" 'sr-speedbar-toggle)
-(my-prefix-set-key "s" 'sr-speedbar-refresh-toggle)
