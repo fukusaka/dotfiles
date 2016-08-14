ed0df3993a2dc3eae0f6883092179cb30085d97f
Shoichi Fukusaka <fukusaka@xa2.so-net.ne.jp>
2016-08-14 21:57:36 +0900

    不要なローカルファイル削除

--- emacs/elisp-addons/muse-3.20-installer.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-: ${EMACS:=/Applications/Emacs.app/Contents/MacOS/Emacs}
-: ${ELCDIR:=~/common/conf/emacs/elisp-emacs24/muse-3.20}
-: ${ADDON:=muse-3.20}
-
-cd ${ADDON}
-make EMACS=${EMACS} ELISPDIR=${ELCDIR} install-bin
-make EMACS=${EMACS} ELISPDIR=${ELCDIR} clean
