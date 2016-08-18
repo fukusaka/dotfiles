08d8c7d3a87c15c1437352f54f80cd7ac7636572
Shoichi Fukusaka <fukusaka@xa2.so-net.ne.jp>
2016-08-18 23:23:32 +0900

    MacOSX 環境の構築スクリプト追加

---
+++ setup/setup-macos-for-me.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+if [ ! -x /usr/local/bin/ansible ]; then
+    echo "install ansible"
+    exit -1
+fi
+
+cd ${HOME}/common/setup/ansible-macos
+
+echo "setup macOS package"
+ansible-playbook -i hosts software.yml -t extra
+ansible-playbook -i hosts software.yml -t personal
