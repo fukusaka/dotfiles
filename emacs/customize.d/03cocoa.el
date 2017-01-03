1c769839ecf41519451d2baad650906613b0fcb9
Shoichi Fukusaka <fukusaka@xa2.so-net.ne.jp>
2017-01-04 03:12:04 +0900

    macos のコピペ対応

--- emacs/customize.d/03cocoa.el
+++ emacs/customize.d/03cocoa.el
@@ -39,5 +39,16 @@
   (setq x-select-enable-clipboard nil)
   (setq x-select-eable-primary t)
   (setq select-active-regions nil)
-  )
 
+  (defun copy-from-osx ()
+    (shell-command-to-string "pbpaste"))
+
+  (defun paste-to-osx (text &optional push)
+    (let ((process-connection-type nil))
+      (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+        (process-send-string proc text)
+        (process-send-eof proc))))
+
+  (setq interprogram-cut-function 'paste-to-osx)
+  (setq interprogram-paste-function 'copy-from-osx)
+  )
