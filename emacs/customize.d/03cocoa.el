1898221d5d03ee55b188ac7d52ce1c7157dcc915
Shoichi Fukusaka <fukusaka@xa2.so-net.ne.jp>
2017-02-05 01:38:02 +0900

    yank over tramp 時の不具合修正

--- emacs/customize.d/03cocoa.el
+++ emacs/customize.d/03cocoa.el
@@ -40,10 +40,21 @@
   (setq select-enable-primary t)
   (setq select-active-regions nil)
 
+  ;; see http://stackoverflow.com/questions/13517910/yank-does-not-paste-text-when-using-emacs-over-ssh
   (defun copy-from-osx ()
-    (shell-command-to-string "pbpaste"))
+    "Handle copy/paste intelligently on osx."
+    (let ((pbpaste (purecopy "/usr/bin/pbpaste")))
+      (if (and (eq system-type 'darwin)
+               (file-exists-p pbpaste))
+          (let ((tramp-mode nil)
+                (default-directory "~"))
+            (shell-command-to-string pbpaste)))))
 
   (defun paste-to-osx (text &optional push)
+    "Handle copy/paste intelligently on osx.
+TEXT gets put into the Macosx clipboard.
+
+The PUSH argument is ignored."
     (let ((process-connection-type nil))
       (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
         (process-send-string proc text)
